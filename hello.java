4450f559407766dea8020fb049b8257ebe2a3a35
debarati <deba@gmail.com>
2023-05-14 12:36:44 +0530

    hello.java changed

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
 class hello{
 
 public static void main(String args[]){
-System.out.println("Hello world Java Jenkins Git.");
+System.out.println("Hello, welcome to the world of Java Jenkins Git.");
 }
 
 }
